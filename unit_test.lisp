aede2a3c8f1849a77df32859cd51b40926478324
Dawei Wei <davwei@microsoft.com>
2023-01-11 22:50:24 -0800

    Add unit test lisp file

---
+++ unit_test.lisp
@@ -0,0 +1,37 @@
+(defun test-+ ()
+  (and
+   (= (+ 1 2) 3)
+   (= (+ 1 2 3) 6)
+   (= (+ -1 -3) -4)))
+
+(test-+)
+
+(defun report-result (result form)
+  (format t "~:[FAIL~;pass~] ... ~a~%" result form))
+
+(defun test-+ ()
+  (report-result (= (+ 1 2) 3) '(= (+ 1 2) 3))
+  (report-result (= (+ 1 2 3) 6) '(= (+ 1 2 3) 6))
+  (report-result (= (+ -1 -3) -4) '(= (+ -1 -3) -4)))
+
+(test-+)
+
+(defmacro check (form)
+  `(report-result ,form ',form))
+
+(defun test-+ ()
+  (check (= (+ 1 2) 3))
+  (check (= (+ 1 2 3) 6))
+  (check (= (+ -1 -3) -4)))
+
+(defmacro check (&body forms)
+  `(progn
+     ,@ (loop for f in forms collect `(report-result ,f ',f))))
+
+(defun test-+ ()
+  (check
+    (= (+ 1 2) 3)
+    (= (+ 1 2 3) 6)
+    (= (+ -1 -3) -4)))
+
+(test-+)
